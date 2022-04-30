72e31db213683627c2d51d2df90f29cf77eb9a21
mhagdorn <magnus.hagdorn@marsupium.org>
2022-04-30 16:25:03 +0100

    add ObjectiveFunctionMisfit

---
+++ ObjectiveFunction_client/objective_function_misfit.py
@@ -0,0 +1,80 @@
+__all__ = ['ObjectiveFunctionMisfit']
+
+import random
+from typing import Mapping
+from pathlib import Path
+import logging
+
+from .objective_function import ObjectiveFunction
+from .parameter import Parameter
+from .common import RunType
+
+
+class ObjectiveFunctionMisfit(ObjectiveFunction):
+    """class maintaining a lookup table for an objective function
+
+    store the model misfit in the database
+
+    :param appname: appname for connecting to objfun server
+    :type appname: str
+    :param secret: password for connecting to objfun server
+    :type secret: str
+    :param study: the name of the study
+    :type study: str
+    :param basedir: the directory in which the lookup table is kept
+    :type basedir: Path
+    :param parameters: a dictionary mapping parameter names to the range of
+        permissible parameter values
+    :param scenario: name of the default scenario
+    :type scenario: str
+    :param db: database connection string
+    :type db: str
+    :param prelim: when True failed parameter look up raises a
+                   PreliminaryRun exception otherwise a
+                   NewRun exception is raised. Default=True
+    :type prelim: bool
+    :param url_base: base URL for ObjectiveFunction server API,
+                     defaults to 'http://localhost:5000/api/'
+    """
+    def __init__(self, appname: str, secret: str,
+                 study: str, basedir: Path,  # noqa C901
+                 parameters: Mapping[str, Parameter],
+                 scenario=None, prelim=True,
+                 url_base='http://localhost:5000/api/'):
+        """constructor"""
+
+        super().__init__(appname, secret, study, basedir,
+                         parameters, scenario=scenario, prelim=prelim,
+                         url_base=url_base, runtype=RunType.MISFIT)
+
+    def setDefaultScenario(self, name):
+        """set the default scenario
+
+        :param name: name of scenario
+        :type name: str
+        """
+        return super().setDefaultScenario(name, runtype=RunType.MISFIT)
+
+
+if __name__ == '__main__':
+    import sys
+    from .parameter import ParameterFloat
+    from .config import ObjFunConfig
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    params = {'a': ParameterFloat(0, -1, 1),
+              'b': ParameterFloat(1, 0, 2, 1e-7),
+              'c': ParameterFloat(-2.5, -5, 0)}
+
+    cfg = ObjFunConfig(Path(sys.argv[1]))
+
+    objfun = ObjectiveFunctionMisfit(cfg.app, cfg.secret,
+                                     "test_study", Path('/tmp'),
+                                     params, scenario="test_scenario_misfit",
+                                     url_base=cfg.baseurl)
+
+    pset1 = {'a': 0, 'b': 1, 'c': -2}
+    pset2 = {'a': 0.5, 'b': 1, 'c': -2}
+    print(objfun.get_result(pset1))
+    #print(objfun.get_result(pset2))
