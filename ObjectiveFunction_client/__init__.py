658553b78d27917a5253677204413888631fbdf6
mhagdorn <magnus.hagdorn@marsupium.org>
2022-05-02 14:30:32 +0100

    load modules

---
+++ ObjectiveFunction_client/__init__.py
@@ -0,0 +1,5 @@
+from .common import *  # noqa: F401,F403
+from .config import *  # noqa: F401,F403
+from .parameter import *   # noqa: F401, F403
+from .objective_function_misfit import *  # noqa: F401,F403
+from .objective_function_residual import *  # noqa: F401,F403
