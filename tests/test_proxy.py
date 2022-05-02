842da3259ffb4d14c4e691e1133819980eee6a9e
mhagdorn <magnus.hagdorn@marsupium.org>
2022-05-02 22:32:23 +0100

    improve coverage

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -3,6 +3,13 @@ import pytest
 from ObjectiveFunction_client.proxy import Proxy
 
 
+def test_proxy_fail(requests_mock, baseurl):
+    requests_mock.register_uri(
+        'GET', baseurl + 'token', status_code=400)
+    with pytest.raises(RuntimeError):
+        Proxy('test', 'test_secret', url_base=baseurl)
+
+
 @pytest.fixture
 def request_token(requests_mock, baseurl):
     requests_mock.register_uri(
@@ -16,5 +23,11 @@ def test_proxy(request_token, baseurl):
 
 def test_proxy_url(request_token, baseurl):
     p = Proxy('test', 'test_secret', url_base=baseurl)
+    url = 'some/string'
+    assert p.url(url) == baseurl + url
+
+
+def test_proxy_url_without(request_token, baseurl):
+    p = Proxy('test', 'test_secret', url_base=baseurl[:-1])
     url = 'some/string'
     assert p.url(url) == baseurl + url
