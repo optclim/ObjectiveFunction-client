a8ec78e6f89b1856f6fb814103a6edcb77e73318
mhagdorn <magnus.hagdorn@marsupium.org>
2022-05-03 13:09:44 +0100

    test ObjectiveFunctionResidual

--- tests/test_ObjectiveFunction_misfit.py
+++ tests/test_ObjectiveFunction_misfit.py
@@ -23,54 +23,66 @@ def objfun():
 
 @pytest.fixture
 def result():
-    return ('misfit', 10)
+    return {'resname': 'misfit',
+            'dbname': 'misfit',
+            'dbvalue': 10,
+            'resvalue': 10}
 
 
 class TestObjectiveFunctionScenarioMisfit(TOFS):
+    def _compare(self, a, b):
+        assert a == b
+
+    def _not_equal(self, a, b):
+        assert a != b
+
     def test_get_result_completed(
             self, requests_mock, baseurl, objectiveA, valuesA, result):
-        resname, misfit = result
+        resname = result['resname']
+        resval = result['resvalue']
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/lookup_run',
             status_code=201, json={
                 'state': LookupState.COMPLETED.name,
-                resname: misfit})
+                result['dbname']: result['dbvalue']})
         res = objectiveA.get_result(valuesA)
         assert res['state'] == LookupState.COMPLETED
-        assert res[resname] == misfit
+        self._compare(res[resname], resval)
 
     def test_get_result(
             self, requests_mock, baseurl, objectiveA, valuesA, result):
-        resname, misfit = result
+        resname = result['resname']
+        resval = result['resvalue']
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/lookup_run',
             status_code=201, json={
                 'state': LookupState.ACTIVE.name,
-                resname: misfit})
+                result['dbname']: result['dbvalue']})
         res = objectiveA.get_result(valuesA)
         assert res['state'] == LookupState.ACTIVE
-        assert res[resname] != misfit
+        self._not_equal(res[resname], resval)
 
     def test_call(self, requests_mock, baseurl, objectiveA, result):
-        resname, misfit = result
+        resval = result['resvalue']
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/lookup_run',
             status_code=201, json={
                 'state': LookupState.COMPLETED.name,
-                resname: misfit})
-        assert objectiveA((0, 1, -2)) == misfit
+                result['dbname']: result['dbvalue']})
+        self._compare(objectiveA((0, 1, -2)), resval)
 
     def test_set_data(self, objectiveA, result):
-        resname, misfit = result
-        assert objectiveA._set_data('x', misfit) == {resname: misfit}
+        assert objectiveA._set_data(
+            {'id': 1}, result['resvalue']) == {
+                result['dbname']: result['dbvalue']}
 
     @pytest.mark.parametrize("code", [400, 403])
     def test_set_result_fail(
             self, requests_mock, baseurl, objectiveA, valuesA, result, code):
-        resname, misfit = result
+        resval = result['resvalue']
         runid = 1
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
@@ -82,11 +94,11 @@ class TestObjectiveFunctionScenarioMisfit(TOFS):
             'PUT', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/runs/{runid}/value', status_code=code)
         with pytest.raises(RuntimeError):
-            objectiveA.set_result(valuesA, misfit)
+            objectiveA.set_result(valuesA, resval)
 
     def test_set_result(
             self, requests_mock, baseurl, objectiveA, valuesA, result):
-        resname, misfit = result
+        resval = result['resvalue']
         runid = 1
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
@@ -98,14 +110,14 @@ class TestObjectiveFunctionScenarioMisfit(TOFS):
             'PUT', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/runs/{runid}/value', status_code=201)
 
-        objectiveA.set_result(valuesA, misfit)
+        objectiveA.set_result(valuesA, resval)
 
     @pytest.mark.parametrize("state",
                              [LookupState.PROVISIONAL, LookupState.NEW,
                               LookupState.ACTIVE, LookupState.COMPLETED])
     def test_set_result_forced(
             self, requests_mock, baseurl, objectiveA, valuesA, result, state):
-        resname, misfit = result
+        resval = result['resvalue']
         runid = 1
         requests_mock.register_uri(
             'POST', baseurl + f'studies/{self.study}/scenarios/'
@@ -117,4 +129,4 @@ class TestObjectiveFunctionScenarioMisfit(TOFS):
             'PUT', baseurl + f'studies/{self.study}/scenarios/'
             f'{self.scenario}/runs/{runid}/value', status_code=201)
 
-        objectiveA.set_result(valuesA, misfit, force=True)
+        objectiveA.set_result(valuesA, resval, force=True)
---
+++ tests/test_ObjectiveFunction_residual.py
@@ -0,0 +1,47 @@
+import pytest
+import numpy
+
+from ObjectiveFunction_client import ObjectiveFunctionResidual
+
+from test_ObjectiveFunction import (  # noqa: F401
+    test_create_fail_create_study, test_create_fail_study,
+    TestObjectiveFunctionExisting, TestObjectiveFunctionBase,
+    TestObjectiveFunctionConstParam, TestObjectiveFunctionNew)
+from test_ObjectiveFunction_misfit import \
+    TestObjectiveFunctionScenarioMisfit as TOFSM
+
+
+@pytest.fixture
+def rundir(tmpdir_factory):
+    res = tmpdir_factory.mktemp("objective_function_residual")
+    return res
+
+
+@pytest.fixture
+def objfun():
+    return ObjectiveFunctionResidual
+
+
+@pytest.fixture
+def result(rundir):
+    fname = rundir / 'residuals_1.npy'
+    with open(fname, 'wb') as f:
+        numpy.save(f, numpy.arange(10))
+    return {'resname': 'residual',
+            'dbname': 'path',
+            'dbvalue': str(fname),
+            'resvalue': numpy.arange(10)}
+
+
+class TestObjectiveFunctionScenarioResidual(TOFSM):
+    def _compare(self, a, b):
+        assert numpy.all(a == b)
+
+    def _not_equal(self, a, b):
+        assert (a.size != b.size) or numpy.all(a != b)
+
+    def test_set_result(
+            self, requests_mock, baseurl, objectiveA, valuesA, result):
+        super().test_set_result(
+            requests_mock, baseurl, objectiveA, valuesA, result)
+        assert objectiveA.num_residuals == 10
